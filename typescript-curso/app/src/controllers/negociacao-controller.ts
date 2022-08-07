c41c7e177898f37d42318f6843b4a829382c9dab
Wanuzia Braga <wanuzia.braga@grupoboticario.com.br>
2022-08-07 20:22:14 -0300

    O @domInjector é um decorator de propriedade que recebe como parâmetro o id do elemento do dom que deve ser buscado e injete a propriedade...

--- typescript-curso/app/src/controllers/negociacao-controller.ts
+++ typescript-curso/app/src/controllers/negociacao-controller.ts
@@ -1,3 +1,4 @@
+import { domInjector } from '../decorators/dom-injector.js';
 import { inspect } from '../decorators/inspect.js';
 import { logarTempoDeExecucao } from '../decorators/logar-tempo-de-execucao.js';
 import { DiasDaSemana } from '../enums/dias-da-semana.js';
@@ -7,8 +8,11 @@ import { MensagemView } from '../views/mensagem-view.js';
 import { NegociacoesView } from '../views/negociacoes-view.js';
 
 export class NegociacaoController {
+    @domInjector('#data')
     private inputData: HTMLInputElement;
+    @domInjector('#quantidade')
     private inputQuantidade: HTMLInputElement;
+    @domInjector('#valor')
     private inputValor: HTMLInputElement;
     
     private negociacoes = new Negociacoes();
@@ -16,9 +20,6 @@ export class NegociacaoController {
     private mensagemView = new MensagemView('#mensagemView');
 
     constructor() {
-        this.inputData = <HTMLInputElement>document.querySelector('#data');
-        this.inputQuantidade = document.querySelector('#quantidade') as HTMLInputElement;
-        this.inputValor = document.querySelector('#valor') as HTMLInputElement;
         this.negociacoesView.update(this.negociacoes);
     }
     @inspect()
