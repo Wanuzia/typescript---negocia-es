157abe05a86868c9801f4eaedb7b04f62f44bd46
Wanuzia Braga <wanuzia.braga@grupoboticario.com.br>
2022-08-12 22:56:19 -0300

    Cria tipagem para dados da API - previne erros de sintaxe e facilita refactor

---
+++ typescript-curso/app/src/interfaces/negociacaoDoDia.ts
@@ -0,0 +1,7 @@
+//uma interface não pode ser instanciada, não é possível criar new a partir dele, não possui constructor.
+//inmterface não é classe não pode ser instanciada
+
+export interface NegociacoesDoDia {
+    montante: number;
+    vezes:number;
+}
