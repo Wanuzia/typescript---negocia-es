750d119617567165896f8cc2d92fb70c146fb5ef
Wanuzia Braga <wanuzia.braga@grupoboticario.com.br>
2022-08-12 22:01:03 -0300

    comentários sobre as alterações que serão feitas a seguir para que o o elemento não seja percorrido toda vez e seja utilizado cache

--- typescript-curso/app/src/decorators/dom-injector.ts
+++ typescript-curso/app/src/decorators/dom-injector.ts
@@ -1,14 +1,17 @@
+//o decorator de propriedade não tem o descriptor, como no decorator de método. 
+
 export function domInjector(seletor: string) {
     return function(target: any, propertyKey: string) {
         console.log(`Modificando prototype ${target.constructor.name} 
         e adicionando getter para a propriedade${propertyKey}`)
+       //getter para ser aplicado mem tempo de execução//toda hora o sendo buscado, queremos buscá-lo somente uma vez, pra usar o elemento em cache;
         const getter = function() {
             const elemento = document.querySelector(seletor);
             console.log(`buscando elemento do DOM com o seletor
             ${seletor} para injetar em ${propertyKey}`);
             return elemento;
         }
-
+//cria o getter
         Object.defineProperty(
             target,
             propertyKey,
