d06a5da59a903f753120bf428df9cfd7ead51dea
Wanuzia Braga <wanuzia.braga@grupoboticario.com.br>
2022-08-12 22:16:51 -0300

    Cria cache no decorator de propriedade domInjector

--- typescript-curso/app/src/decorators/dom-injector.ts
+++ typescript-curso/app/src/decorators/dom-injector.ts
@@ -1,14 +1,25 @@
 //o decorator de propriedade não tem o descriptor, como no decorator de método. 
 
+//a function que vai criar e aplicar o getter de uma vez só para cada propriedade
+//o getter sempre vai verificar se o elemento existe ou não // decorator versátil e performático
 export function domInjector(seletor: string) {
     return function(target: any, propertyKey: string) {
         console.log(`Modificando prototype ${target.constructor.name} 
         e adicionando getter para a propriedade${propertyKey}`)
        //getter para ser aplicado mem tempo de execução//toda hora o sendo buscado, queremos buscá-lo somente uma vez, pra usar o elemento em cache;
-        const getter = function() {
-            const elemento = document.querySelector(seletor);
-            console.log(`buscando elemento do DOM com o seletor
-            ${seletor} para injetar em ${propertyKey}`);
+       
+       
+       let elemento: HTMLElement;
+
+       //o getter é atribuído para o define property, sempre lembra o valor da variável definida no escopo anterior
+       //busca o elemento apenas uma vez, depois apenas o renderiza novamente
+       const getter = function() {
+        if (!elemento) {
+            elemento = <HTMLElement>document.querySelector(seletor)
+           console.log(`buscando elemento do DOM com o seletor
+            ${seletor} para injetar em ${propertyKey}`); 
+        }
+        
             return elemento;
         }
 //cria o getter
